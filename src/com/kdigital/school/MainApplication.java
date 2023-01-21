505496dbef6c8fa181f4970f6879af79d6b18de2
merge-simpson <letsdev@kakao.com>
2023-01-21 18:02:02 +0900

    test commit to push

--- src/com/kdigital/school/MainApplication.java
+++ src/com/kdigital/school/MainApplication.java
@@ -3,5 +3,6 @@ package com.kdigital.school;
 public final class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is github push test");
     }
 }
