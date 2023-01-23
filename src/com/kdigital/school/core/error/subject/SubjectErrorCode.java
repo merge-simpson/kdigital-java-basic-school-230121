a227c4e815f77473f13835cb3ce8ba6e5181fb3f
merge-simpson <letsdev@kakao.com>
2023-01-23 15:05:07 +0900

    feat(subject): add error code

--- src/com/kdigital/school/core/error/subject/SubjectErrorCode.java
+++ src/com/kdigital/school/core/error/subject/SubjectErrorCode.java
@@ -4,6 +4,8 @@ import com.kdigital.school.core.error.ErrorCode;
 
 public enum SubjectErrorCode implements ErrorCode {
     DUPLICATED_SUBJECT_CODE("과목 코드가 이미 존재합니다."),
+    BLANK_SUBJECT_CODE("Subject code는 null이 될 수 없습니다."),
+    UNNAMED_SUBJECT("과목 이름이 입력되어야 합니다."),
     SCORE_TYPE_IS_NULL("Score type cannot be null.");
 
     public final String MESSAGE;
